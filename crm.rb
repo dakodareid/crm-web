4b154ef26c9ccdb366d2094df002ce22e8645448
dakodareid <YOUR EMAIL ADDRESSdakodareid@gmail.com>
2014-10-30 13:08:54 -0400

    fake contact added, and set as variable. show_contacts.erb added

--- crm.rb
+++ crm.rb
@@ -3,6 +3,7 @@ require_relative 'rolodex'
 require_relative 'contact'
 
 $rolodex= Rolodex.new
+@@rolodex.add_contact(Contact.new("Dakoda", "Reid", "Dakoda@gmail.com", "none"))
 
 get '/' do 
 	@crm_app_name = "My CRM"
@@ -17,8 +18,16 @@ get '/contacts/new' do
 	erb :contacts_new
 end
 
+get '/contact/1000' do
+	@contact = @@rolodex.find(1000)
+	erb :show_contact
+end
+
+contact = @@rolodex.find(1000)
+
 post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(new_contact)
 	redirect to('/contacts')
+	
 end
